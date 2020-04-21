d1bcf4bd4fb7235e95b390c4b1c10ec557296b21
Dhruvil Dave <dhruvil@gkmit.co>
2020-04-21 16:32:22 +0530

    image added

--- recipe/views/views_recipe.py
+++ recipe/views/views_recipe.py
@@ -1,9 +1,12 @@
-from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from recipe.models.models_recipe import Recipe
-from recipe.serializers.serializers_recipe import RecipeSerializer, RecipeDetailSerializer
+from recipe.serializers.serializers_recipe import RecipeSerializer,\
+                    RecipeDetailSerializer, RecipeImageSerializer
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """Manage recipes in the database"""
@@ -25,5 +28,27 @@ class RecipeViewSet(viewsets.ModelViewSet):
       """Return appropriate serializer class"""
       if self.action == 'retrieve':
           return RecipeDetailSerializer
-
+      elif self.action == 'upload_image':
+        return RecipeImageSerializer
       return self.serializer_class
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+      """Upload an image to a recipe"""
+      recipe = self.get_object()
+      serializer = self.get_serializer(
+          recipe,
+          data=request.data
+      )
+
+      if serializer.is_valid():
+          serializer.save()
+          return Response(
+              serializer.data,
+              status=status.HTTP_200_OK
+          )
+
+      return Response(
+          serializer.errors,
+          status=status.HTTP_400_BAD_REQUEST
+      )
